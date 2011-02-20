6e07ac702dc944d8012b097e0db06009c4c74e7d
d0n3val <d0n3val@gmail.com>
2011-02-20 01:38:20 +0100

    Now set_dirs calls make.bat for compiling

--- set_dirs.bat
+++ set_dirs.bat
@@ -1,5 +1,7 @@
+# Setup include and lib folder before compiling
+
 set include=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Include;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include
 
 set lib=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Lib;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib
 
-cl /nologo /WL /Od /GF /Gm- /GL- /Y- /TP /I. code/*.c /MTd /Fetwk
+call make.bat
