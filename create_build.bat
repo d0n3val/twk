1e4597c0deada691e0e21de6ed2e0970fecd2452
d0n3val <d0n3val@gmail.com>
2011-02-20 02:37:17 +0100

    Simple batch file for build creation

---
+++ create_build.bat
@@ -0,0 +1,12 @@
+# Compiles and creates a build .zip file
+
+call set_dirs.bat
+
+mkdir twk
+xcopy *.dll twk
+xcopy twk.exe twk
+xcopy /i data twk\data
+7za.exe a -r twk_%date:~-4%_%date:~3,2%_%date:~0,2%.zip twk
+del /s /q twk
+rmdir twk\data
+rmdir twk
