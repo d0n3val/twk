25be4abff7a2a46ccc5d155882fc75bef646d253
d0n3val <d0n3val@gmail.com>
2011-06-05 12:57:49 +0200

    Removing mmgr

--- code/main.c
+++ code/main.c
@@ -20,7 +20,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "mmgr.h"
 #if _WIN32
 # include "bass/bass.h"
 # include "mxml/mxml.h"
--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-cl /nologo /WL /Od /GF /Gm- /GL- /Y- /TP /I. code/*.c /MTd /Fetwk
+cl /nologo /WL /Od /GF /Gm- /GL- /Y- /TP /I. code/main.c /MTd /Fetwk /EHsc
